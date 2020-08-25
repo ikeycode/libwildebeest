3f540a815c923c90412a10c50d6f181fff528527
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-25 23:37:00 +0100

    Initial version of strndupa build-time replacement
    Our aim is to replace all calls to strndupa with our version, which
    is only going to work when the symbol is undefined, i.e. in the case
    of musl.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ src/string/lwb_string.h
@@ -0,0 +1,40 @@
+/*
+ * This file is part of libwildebeest
+ *
+ * Copyright © 2020 Serpent OS Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/* We might be used with old compiler settings. */
+#ifndef _LWB_STRING_H
+#define _LWB_STRING_H
+
+#if defined(_XOPEN_SOURCE) || defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
+
+#include <stddef.h>
+
+/**
+ * Provide the standard strndupa signature
+ */
+char *strndupa(const char *s, size_t n);
+
+#endif
+
+#endif /* _LWB_STRING_H */
