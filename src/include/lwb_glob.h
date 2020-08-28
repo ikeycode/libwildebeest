01831685c47fd6ff5809b8fa792b6102ac2bf731
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-28 19:08:25 +0100

    include: Add missing glob defines
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- src/include/lwb_glob.h
+++ src/include/lwb_glob.h
@@ -35,6 +35,33 @@
 #include <stddef.h>
 #include <sys/stat.h>
 
+#define GLOB_ERR (1 << 0)
+#define GLOB_MARK (1 << 1)
+#define GLOB_NOSORT (1 << 2)
+#define GLOB_DOOFFS (1 << 3)
+#define GLOB_NOCHECK (1 << 4)
+#define GLOB_APPEND (1 << 5)
+#define GLOB_NOESCAPE (1 << 6)
+#define GLOB_PERIOD (1 << 7)
+
+#define GLOB_MAGCHAR (1 << 8)
+#define GLOB_ALTDIRFUNC (1 << 9)
+#define GLOB_BRACE (1 << 10)
+#define GLOB_NOMAGIC (1 << 11)
+#define GLOB_TILDE (1 << 12)
+#define GLOB_ONLYDIR (1 << 13)
+#define GLOB_TILDE_CHECK (1 << 14)
+
+#define __GLOB_FLAGS                                                                               \
+        (GLOB_ERR | GLOB_MARK | GLOB_NOSORT | GLOB_DOOFFS | GLOB_NOESCAPE | GLOB_NOCHECK |         \
+         GLOB_APPEND | GLOB_PERIOD | GLOB_ALTDIRFUNC | GLOB_BRACE | GLOB_NOMAGIC | GLOB_TILDE |    \
+         GLOB_ONLYDIR | GLOB_TILDE_CHECK)
+#define GLOB_NOSPACE 1
+#define GLOB_ABORTED 2
+#define GLOB_NOMATCH 3
+#define GLOB_NOSYS 4
+#define GLOB_ABEND GLOB_ABORTED
+
 /**
  * GNU-compatible glob_t structure
  */
